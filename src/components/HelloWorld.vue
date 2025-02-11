6b8a396e499c594ea589fc54fd84624bb0236d4c
KIPOKA <ladykipoka@gmail.com>
2025-02-11 19:47:18 +0200

    Version 2: Changed text color to blue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-red-600">
+    <h1 class="text-4xl font-bold text-blue-600">
       Hello, Tailwind CSS with Vue!
     </h1>
   </div>
