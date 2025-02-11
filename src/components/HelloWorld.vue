865500462cd42215906b781d65d09ca0b5598c23
KIPOKA <ladykipoka@gmail.com>
2025-02-11 19:48:42 +0200

    Version 4: Added background image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-red-600">
-      Hello, Tailwind CSS with Vue!
-    </h1>
-    <button class="mt-4 px-6 py-2 bg-green-500 text-white rounded">
-      Click Me
-    </button>
+  <div
+    class="flex items-center justify-center h-screen bg-cover bg-center"
+    style="background-image: url('https://via.placeholder.com/150')"
+  >
+    <h1 class="text-4xl font-bold text-white">Hello, Tailwind CSS with Vue!</h1>
   </div>
 </template>
 
