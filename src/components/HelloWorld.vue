fba55e73149e48dc16214730f17e4f663b7804ac
KIPOKA <ladykipoka@gmail.com>
2025-03-05 21:50:36 +0200

    version commit 5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-green-500 animate-pulse">
+  <div
+    class="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-800"
+  >
+    <h1 class="text-4xl font-bold text-purple-500 dark:text-yellow-300">
       {{ msg || "Hello, Tailwind CSS with Vue!" }}
     </h1>
   </div>
